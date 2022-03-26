fece61d39ac05420edd757333cb69616a936f28e
dogring <changh232@naver.com>
2022-03-26 23:56:19 +0900

    move parser and session to main

--- src/mainkp.js
+++ src/mainkp.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 const PORT = 8880;
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+var session = require('express-session');
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:'secret key'
+}));
+
 app.use(require('./routkp'));
 
 app.listen(PORT, () => {
