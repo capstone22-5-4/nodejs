dffe34fcdaf1677438adb0c1e7423f679bc42942
dogring <changh232@naver.com>
2022-03-30 00:24:17 +0900

    create with email and nickname not overlap

--- src/routkp.js
+++ src/routkp.js
@@ -3,6 +3,7 @@ const mydb = require('./dbModel');
 const passport = require('passport');
 
 const crypto = require('crypto');
+const Op = require('sequelize').Op;
 
 router.post('/signup', signup);
 router.delete('/signout', signout);
@@ -17,37 +18,43 @@ router.get('/logout', (req, res) => {
 });
 
 router.get('/list', getlist);
-router.get('/book/:user_id',getbook);
+router.get('/detail',getdetail);
 
 module.exports = router;
 
 
 function signup(req,res){
-    // const id = req.body.id.replace(/ /g,"");
-    // const password = req.body.password.replace(/ /g,"");
-    // const phone = req.body.phone;
-    const {id, pw, phone} = req.body;
-    if(id && pw && phone){
-        // const salt = crypto.randomBytes(10).toString('base64');
-        const salt = 'sampleSalt'
+    const {email, pw, name, nickname} = req.body;
+    if(email && pw && name && nickname){
+        const salt = crypto.randomBytes(10).toString('base64');
         const hash_pw = crypto
         .createHash('sha256')
         .update(pw + salt)
         .digest('hex');
 
         mydb.users.findOrCreate({
-            where: {user_id : id},
+            where: {
+                [Op.or] : [{email : email}, {nickname : nickname}] 
+            },
+            attributes: ['nickname', 'email'],
             defaults:{
+                email : email,
+                nickname : nickname,
+                name : name,
                 salt_key: salt,
                 password :hash_pw,
-                phone:phone
             }
         }).then((results) =>{
-            if(!results[1])
-                res.status(202).send(`${id} is already exists`);
+            if(!results[1]){
+                if ( email == results[0].dataValues.email)
+                    res.status(202).send(`email ${email} is already exists`);
+                else if ( nickname == results[0].dataValues.email)
+                    res.status(202).send(`nickname ${nickname} is already exists`);
+                else res.status(202).send(`something wrong in singup`);
+            }
             else {
-                res.status(200).send(`${id} has been created.`);
-                console.log(`ip : ${req.ip}\ncreate ${id}`);
+                res.status(200).send(`${email} has been created.`);
+                console.log(`ip : ${req.ip}\ncreate ${email}`);
             }
         });
     } else {
@@ -69,16 +76,15 @@ function signout(req,res){
     }
 }
 
-function getbook(req,res){
+function getdetail(req,res){
     var user = req.user;
-    console.log('user',user);
-    console.log(req.params.user_id)
-    if (user && (user == req.params.user_id)){
+    if (user){
         mydb.users.findOne({
             where: {id : user},
-            attributes: ['user_id', 'phone']
+            attributes: ['nickname', 'email']
         })
         .then((results, rejected) => {
+            console.log(results);
             if(results)
                 res.status(200).send(results.dataValues);
             else
@@ -91,7 +97,7 @@ function getbook(req,res){
 
 function getlist(req, res){
     mydb.users.findAll({
-            attributes : ['user_id','phone']
+            attributes : ['email','nickname']
         }).then(
         (results) => {
             console.log('list request');
