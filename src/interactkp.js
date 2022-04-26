53e1ea58f1febe2b46c79d06c3f59a8c45a77a2b
dogring <changh232@naver.com>
2022-04-26 17:05:06 +0900

    change order and root

--- src/interactkp.js
+++ src/interactkp.js
@@ -5,19 +5,45 @@ const passport = require('passport');
 
 const fs = require('fs');
 
-router.use(express.static(process.env.PWD+'/base'));
 
+router.get('/rank', ranklist);
 router.post('/addscore', addscore);
-router.post('/addfood', addfood);
+router.post('/buyfood', buyfood);
+router.get('/shop', foodlist);
 router.get('/analmal',(req,res) =>{
-    res.sendFile(process.env.PWD+'/base/index.html');
+    console.log(__dirname);
+    console.log('/base/index.html');
+    res.sendFile('/base/index.html', { root : process.env.PWD });
 });
+
+router.use(express.static(process.env.PWD+'/base'));
 
 module.exports = router;
 
+async function ranklist(req,res){
+    var user = req.user;
+    var ranking
+    if (user){
+        ranking['I'] = await mydb.score.findOne({
+            where : {id : user},
+            attributes : ['score']
+        })
+    } else { ranking['I'] = null}
+    mydb.score.findAll({
+        
+    });
+}
+
+function foodlist(req,res){
+
+}
+
+
+
 function addscore(req,res){
+
 }
 
-function addfood(req,res){    
-}
+function buyfood(req,res){
 
+}
