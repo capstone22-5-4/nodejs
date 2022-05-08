7003526a6e8159b49304d756c15658d1b7777d38
dogring <changh232@naver.com>
2022-05-08 13:10:55 +0900

    credit, score get function

--- src/interactkp.js
+++ src/interactkp.js
@@ -7,9 +7,10 @@ const fs = require('fs');
 
 
 router.get('/rank', ranklist);
-router.post('/addscore', addscore);
 router.post('/buyfood', buyfood);
 router.get('/shop', foodlist);
+router.get('/credit', getCredit);
+router.get('/score', getScore);
 router.get('/analmal',(req,res) =>{
     res.sendFile('/home/ubuntu/nodejs/base/index.html');
     // res.sendFile('/base/index.html', { root : process.env.PWD });
@@ -33,14 +34,35 @@ async function ranklist(req,res){
         
     });
 }
-
-function foodlist(req,res){
-
+function getCredit(req, res){
+    var user = req.user;
+    if (user){
+        mydb.score.findOne({
+            where : {id: user},
+            attributes : ['credit']
+        }).then((results) => {
+            if (results){
+                      res.status(200).send(String(results.credit));
+            }else   { res.status(202).send('no user');}
+        });
+    } else          { res.status(401).send('login first');}
 }
 
+function getScore(req, res){
+    var user = req.user;
+    if (user){
+        mydb.score.findOne({
+            where : {id: user},
+            attributes : ['score']
+        }).then((results) => {
+            if (results){
+                      res.status(200).send(String(results.credit));
+            }else   { res.status(202).send('no user');}
+        });
+    } else          { res.status(401).send('login first');}
+}
 
-
-function addscore(req,res){
+function foodlist(req,res){
 
 }
 
