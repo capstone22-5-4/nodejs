7459ad6d09013623e2913af9840d0ebba154fc7b
dogring <changh232@naver.com>
2022-05-24 23:13:56 +0900

    change food kinds and erase trash tables

--- src/dbModel.js
+++ src/dbModel.js
@@ -44,33 +44,13 @@ var score = sequelize.define('score', {
                 defaultValue : 0 }
 },{ createdAt : false, updatedAt : false });
 
-var animals = sequelize.define('animals', {
-    id : {          type : Sequelize.INTEGER,
-                    primaryKey : true,
-                    autoIncrement : true },
-    animal_name : { type : Sequelize.STRING,
-                    allowNull : false}
-}, { createdAt : false, updatedAt : false });
-
-var foods = sequelize.define('foods', {
-    id : {          type : Sequelize.INTEGER,
-                    primaryKey : true,
-                    autoIncrement : true },
-    food_name : {   type : Sequelize.STRING,
-                    allowNull : false },
-    image_url : {   type : Sequelize.STRING,
-                    allowNull : false },
-    food_cost : {   type : Sequelize.INTEGER,
-                    allowNull : false }
-}, { createdAt : false, updatedAt : false });
-
 var has_foods = sequelize.define('user_foods', {
     id : {      type : Sequelize.INTEGER,
                 primaryKey : true },
     foods : {   type : Sequelize.JSON, 
                 defaultValue : 
-                {"사과":0, "대나무":0, "물고기":0, 
-                "사료":0, "소고기":0, "지렁이":0, "풀":0 } }
+                {"사과":0, "과일":0, "물고기":0, 
+                "사료":0, "소고기":1, "지렁이":0, "풀":0 } }
 }, { createdAt : false });
 
 var user_achivement = sequelize.define('user_achive', {
@@ -94,13 +74,30 @@ var gpsData = sequelize.define('gps', {
 }, { updatedAt : false});
 
 
-user.sync().then(() => {console.log('User table connected');});
-image.sync().then(() => {console.log('Image table connected');});
-score.sync().then(() => {console.log('user score table connected');});
-animals.sync().then(() => {console.log('base animal table connected');});
-foods.sync().then(() => {console.log('base food table connected');});
-has_foods.sync().then(() => {console.log('user foods table connected');});
-gpsData.sync().then(() => console.log('gps table connected'));
+user.sync({force:true}).then(() => {console.log('User table connected');
+    user.create({
+        email : 'first@abcd.efg',
+        nickname : 'sample',
+        name : 'caps',
+        salt_key : 'sampleSalt',
+        password : 'ac8d85f18cb8fd8e7f7b4dd0c23cf0a07675b3bf3e491bc62be070ee3699b50d',
+    });});
+image.sync({force:true}).then(() => {console.log('Image table connected');
+    image.create({
+        id : 1,
+        animals : { '거북' : '1_dummy.jpg'},
+    });});
+score.sync({force:true}).then(() => {console.log('user score table connected');
+    score.create({
+        id : 1,
+        credit : 10,
+        score : 10
+    });});
+has_foods.sync({force:true}).then(() => {console.log('user foods table connected');
+    has_foods.create({
+        id : 1
+    });});
+gpsData.sync({force:true}).then(() => console.log('gps table connected'));
 user_achivement.sync({force:true}).then(() => {console.log('base achivemnets table connected');});
 
 
@@ -108,8 +105,6 @@ module.exports ={
     users : user,
     images : image,
     score : score,
-    animals : animals,
-    foods : foods,
     has_foods : has_foods,
     gps : gpsData,
     user_achivement : user_achivement
